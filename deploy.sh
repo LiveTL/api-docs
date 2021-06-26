ed7c68c9491ed0529e613dd3e3f1386ec0d2b6eb
Grumpy Bear <grumpybear4257@gmail.com>
2021-06-26 14:54:30 -0600

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 echo "pulling changes and building"
 git pull
 bundle exec middleman build
 
 echo "removing old version and copying new"
-rm -rf /opt/livetl-api-docs/*
-cp -r ./build/* /opt/livetl-api-docs/
+sudo rm -rf /opt/livetl-api-docs/*
+sudo cp -r ./build/* /opt/livetl-api-docs/
+sudo chown -R www-data:www-data /opt/livetl-api-docs/
 
 echo "done"
